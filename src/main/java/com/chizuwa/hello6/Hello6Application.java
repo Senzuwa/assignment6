1196d19fd4dde57788dce9d6481c20ef268dba23
Senzuwa <azkuth@icloud.com>
2023-12-18 21:38:50 +0900

    first commit

---
+++ src/main/java/com/chizuwa/hello6/Hello6Application.java
@@ -0,0 +1,14 @@
+package com.chizuwa.hello6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello6Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/chizuwa/hello6/Hello6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chizuwa.hello6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
